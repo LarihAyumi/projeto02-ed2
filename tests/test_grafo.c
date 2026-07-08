0383f59c5b9b9000f792016e3273ee7fb0a3515e
LarihAyumi <larissa.ayumi.costa@uel.br>
2026-07-07 22:03:37 -0300

    adição de testes para novos componentes para comando exp

--- tests/test_grafo.c
+++ tests/test_grafo.c
@@ -183,6 +183,90 @@ void testGrafoComponentesConexosBBox(void) {
     destruirGrafo(grafo);
 }
 
+void testGrafoExpansaoViaria(void) {
+    Grafo* grafo = criarGrafo();
+    int qtd;
+
+    TEST_ASSERT_NOT_NULL(grafo);
+
+    inserirVertice(grafo, "v1", 0.0, 0.0);
+    inserirVertice(grafo, "v2", 100.0, 0.0);
+    inserirVertice(grafo, "v3", 200.0, 0.0);
+
+    inserirAresta(grafo, "v1", "v2", "cep1", "cep2", 10.0, 4.0, "Rua_A");
+    inserirAresta(grafo, "v2", "v3", "cep2", "cep3", 20.0, 3.0, "Rua_B");
+    inserirAresta(grafo, "v1", "v3", "cep1", "cep3", 100.0, 2.0, "Rua_C");
+
+    qtd = aplicarExpansaoViaria(grafo, 5.0);
+
+    TEST_ASSERT_EQUAL_INT(2, qtd);
+
+    TEST_ASSERT_EQUAL_INT(1, arestaFoiExpandida(grafo, "v1", "v2"));
+    TEST_ASSERT_EQUAL_INT(1, arestaFoiExpandida(grafo, "v2", "v3"));
+    TEST_ASSERT_EQUAL_INT(0, arestaFoiExpandida(grafo, "v1", "v3"));
+
+    TEST_ASSERT_EQUAL_INT(60, (int)(obterVelocidadeAresta(grafo, "v1", "v2") * 10 + 0.5));
+    TEST_ASSERT_EQUAL_INT(45, (int)(obterVelocidadeAresta(grafo, "v2", "v3") * 10 + 0.5));
+    TEST_ASSERT_EQUAL_INT(20, (int)(obterVelocidadeAresta(grafo, "v1", "v3") * 10 + 0.5));
+
+    destruirGrafo(grafo);
+}
+
+void testGrafoDesenhaExpansaoViariaSvg(void) {
+    Grafo* grafo = criarGrafo();
+    FILE* svg;
+    FILE* arq;
+    char buffer[300];
+    int achouLinha = 0;
+    int achouVermelho = 0;
+    int achouGrossa = 0;
+
+    TEST_ASSERT_NOT_NULL(grafo);
+
+    inserirVertice(grafo, "v1", 0.0, 0.0);
+    inserirVertice(grafo, "v2", 100.0, 0.0);
+    inserirVertice(grafo, "v3", 200.0, 0.0);
+
+    inserirAresta(grafo, "v1", "v2", "cep1", "cep2", 10.0, 4.0, "Rua_A");
+    inserirAresta(grafo, "v2", "v3", "cep2", "cep3", 20.0, 3.0, "Rua_B");
+    inserirAresta(grafo, "v1", "v3", "cep1", "cep3", 100.0, 2.0, "Rua_C");
+
+    aplicarExpansaoViaria(grafo,5.0);
+
+    svg = fopen("teste_expansao.svg", "w");
+    TEST_ASSERT_NOT_NULL(svg);
+
+    desenharExpansaoViariaSvg(grafo, svg);
+
+    fclose(svg);
+
+    arq = fopen("teste_expansao.svg", "r");
+    TEST_ASSERT_NOT_NULL(arq);
+
+    while (fgets(buffer, sizeof(buffer), arq) != NULL) {
+        if (strstr(buffer, "<line") != NULL) {
+            achouLinha = 1;
+        }
+
+        if (strstr(buffer, "red") != NULL) {
+            achouVermelho = 1;
+        }
+
+        if (strstr(buffer, "stroke-width=\"4\"") != NULL) {
+            achouGrossa = 1;
+        }
+    }
+
+    fclose(arq);
+    remove("teste_expansao.svg");
+
+    TEST_ASSERT_TRUE(achouLinha);
+    TEST_ASSERT_TRUE(achouVermelho);
+    TEST_ASSERT_TRUE(achouGrossa);
+
+    destruirGrafo(grafo);
+}
+
 int main(void) {
     UNITY_BEGIN();
 
@@ -195,6 +279,8 @@ int main(void) {
     RUN_TEST(testGrafoAtualizaVelocidadeRegiao);
     RUN_TEST(testGrafoComponentesConexos);
     RUN_TEST(testGrafoComponentesConexosBBox);
+    RUN_TEST(testGrafoExpansaoViaria);
+    RUN_TEST(testGrafoDesenhaExpansaoViariaSvg);
 
     return UNITY_END();
 }
